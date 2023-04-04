0fc12d2bb906d4b9e88ad72181fe03aedcae7d72
Sohil <rahilsathwara410@gmail.com>
2023-04-04 23:50:39 +0530

    demo for the read data from excel and download

--- demo-excel-operations/src/main/java/com/spring/practice/repository/QuestionRespository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.practice.repository;
-
-import com.spring.practice.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRespository extends JpaRepository<Question, Long> {
-}
